352e47d2ce03ce670434d6eca160a86c63d193ec
Shilad Sen <shilad@gmail.com>
2018-03-09 22:51:30 -0600

    Preparing to run on twenty langs.

--- create_align_input.py
+++ create_align_input.py
@@ -183,4 +183,7 @@ def read_word2vec(path):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main('./input', './output')
+    if len(sys.argv) != 3:
+        sys.stderr.write('usage: %s path_in path_out' % (sys.argv[0]))
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
