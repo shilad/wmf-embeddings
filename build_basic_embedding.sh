866396a0e6839c174be1422657b34dd9953b798c
Shilad Sen <shilad@nokomis.macalester.edu>
2018-03-17 15:44:51 -0400

    Added algorithm switch.

--- build_basic_embedding.sh
+++ build_basic_embedding.sh
@@ -11,7 +11,8 @@
 set -e
 set -x
 
-langs="en,de,simple,ar,az,bg,ca,cs,da,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,hu,id,it,ja,kk,ko,lt,ms,nl,nn,no,pl,pt,ro,ru,sk,sl,sr,sv,tr,uk,vi,vo,war,zh"
+languages="en,de,simple,ar,az,bg,ca,cs,da,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,hu,id,it,ja,kk,ko,lt,ms,nl,nn,no,pl,pt,ro,ru,sk,sl,sr,sv,tr,uk,vi,vo,war,zh"
+algorithm=fasttext
 name=vectors.fasttext.txt
 script_args=""
 
@@ -20,12 +21,21 @@ while [[ $# -gt 0 ]]; do
 
     case $key in
         --languages)
-        langs="$2"
+        languages="$2"
         shift # past argument
         shift # past value
         ;;
         --name)
         name="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --algorithm)
+        algorithm="$2"
+        if [ $algorithm != "fasttext" ] && [ $algorithm != "doc2vec" ]; then
+	    echo "unknown algorithm: $algorithm. must be fasttext or doc2vec" >&2
+	    exit 1
+	fi
         shift # past argument
         shift # past value
         ;;
@@ -38,6 +48,7 @@ while [[ $# -gt 0 ]]; do
         echo "
         Usage: [--languages en,de,...]
                [--name vectors.fasttext.txt]
+               [--algorithm fasttext|doc2vec]
                [-- extra arguments to script]
        " >&2
        exit 1
@@ -46,12 +57,14 @@ while [[ $# -gt 0 ]]; do
 done
 
 
-echo building word2vec model $name for languages $langs with extra args of $script_args >&2
+echo building $algorithm model $name for languages $languages with extra args of $script_args >&2
 
 
 function do_lang() {
     name=$1
     lang=$2
+    algorithm=$3
+    shift
     shift
     shift
     extra_args=$@
@@ -59,7 +72,7 @@ function do_lang() {
     mkdir -p ${path_vecs}
     aws s3 cp s3://wikibrain/w2v2/$lang/corpus.txt.bz2 ${path_vecs}/
     aws s3 cp s3://wikibrain/w2v2/$lang/dictionary.txt.bz2 ${path_vecs}/
-    python36 -m wmf_embed.train.train_fasttext \
+    python36 -m wmf_embed.train.train_${algorithm} \
                 --corpus ${path_vecs}/ \
                 --output ${path_vecs}/$name \
                 $extra_args 2>&1 | tee ${path_vecs}/log.txt
@@ -70,4 +83,4 @@ function do_lang() {
 
 export -f do_lang
 
-echo $langs | tr ',' '\n' | parallel -j 6 --line-buffer do_lang $name $script_args '{}'
+echo $languages | tr ',' '\n' | parallel -j 6 --line-buffer do_lang $name '{}' $algorithm $script_args
