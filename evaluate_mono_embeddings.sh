9278de886a28306c49c8f23f699a3c8854880e27
Shilad Sen <shilad@gmail.com>
2018-04-05 14:18:53 -0500

    Tried to clean up mono eval script.

--- evaluate_mono_embeddings.sh
+++ evaluate_mono_embeddings.sh
@@ -11,7 +11,7 @@
 set -e
 set -x
 
-languages="en,de,simple,ar,az,bg,ca,cs,da,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,hu,id,it,ja,kk,ko,lt,ms,nl,nn,no,pl,pt,ro,ru,sk,sl,sr,sv,tr,uk,vi,vo,war,zh"
+languages="en,de,simple,es,fa,it"
 algorithm=fasttext
 name=vectors.fasttext.txt
 script_args=""
@@ -66,19 +66,21 @@ function do_lang() {
     extra_args=$@
     path_vecs=./vecs/${name}/${lang}
     mkdir -p ${path_vecs}
-#    aws s3 cp s3://wikibrain/w2v2/$lang/${name}.bz2 ${path_vecs}/
-#    pbunzip2 ${path_vecs}/${name}.bz2
+    aws s3 cp s3://wikibrain/w2v2/$lang/${name}.bz2 ${path_vecs}/
+    pbunzip2 ${path_vecs}/${name}.bz2
     python3 -m wmf_embed.model.evaluate_monolingual_embeddings \
                 --path ${path_vecs}/${name} \
                 --lang ${lang} \
-                $extra_args 2>&1 | tee ${path_vecs}/log.txt
-#    pbzip2 ${path_vecs}/$name
-#    aws s3 cp ${path_vecs}/${name}.bz2 s3://wikibrain/w2v2/$lang/
-#    rm -rf ${path_vecs}/
+                $extra_args 2>&1 | tee ${path_vecs}/eval.mono.${name}.txt
+    aws s3 cp ${path_vecs}/eval.mono..${name}.txt s3://wikibrain/w2v2/$lang/
+    rm -rf ${path_vecs}/
 }
 
 export -f do_lang
 
-for lang in $languages; do
-    do_lang $name $lang $script_args
-done
+
+echo $languages |
+tr ',' '\n' |
+parallel -j ${jobs} --line-buffer do_lang $name '{}' $script_args
+'{}'  $script_args
+
